b590e703152a72f97a12aabb0b2009363ef93071
Muhammad Shahriar Hasan <muhammad15-13854@diu.edu.bd>
2024-03-07 13:27:42 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Header from './Components/Header/Header'
+import Blogs from './Components/Blogs/Blogs'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+      
+      
+      
+    </>
+  )
+}
+
+export default App
