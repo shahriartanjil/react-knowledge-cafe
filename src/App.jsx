b17a858deec1edef1a83f040481fdebfcef93f6a
Muhammad Shahriar Hasan <muhammad15-13854@diu.edu.bd>
2024-03-12 15:42:12 +0600

    added bookmarks and blog

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
 import Header from './Components/Header/Header'
 import Blogs from './Components/Blogs/Blogs'
+import BookMarks from './Components/Bookmarks/BookMarks'
 
 function App() {
 
   return (
     <>
       <Header></Header>
+      <div className='md:flex '>
       <Blogs></Blogs>
+      <BookMarks></BookMarks>
+
+      </div>
       
       
       
